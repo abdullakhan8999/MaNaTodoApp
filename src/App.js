759583c3882b5b8f1ec838db4ce0ef2339f504ef
Patan AbdullaKhan <abdullakhan8999@gmail.com>
2023-02-06 15:28:27 +0530

    MaNaTodoApp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
-import Navbar from "./component/Navbar";
-import TodoInput from "./component/TodoInput";
-import TodoList from "./component/TodoList";
+import Navbar from "./components/Navbar";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import About from "./components/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+
 export default function App() {
   document.body.style.backgroundColor = "#202020";
   document.body.style.color = "white";
 
   return (
-    <>
+    <Router>
       <Navbar Brand={"MaNaToDo"} />
-      <TodoInput />
-    </>
+      <Routes>
+        <Route exact path="/home" element={<TodoInput />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./About.css";
+
+export default function About() {
+  return (
+    <>
+      <h1 className="about_h1">About MaNaToDoApp</h1>
+      <p className="about_p">
+        MaNaToDoApp is a simple and intuitive to-do app that helps you stay
+        organized and on top of your tasks. With MaNaToDoApp, you can create and
+        manage a list of items to be completed, set due dates, prioritize tasks,
+        categorize tasks, and receive reminders. The goal of MaNaToDoApp is to
+        provide a single, centralized location for you to keep track of what you
+        need to do and stay productive. Download MaNaToDoApp now and start
+        getting things done!
+      </p>
+    </>
+  );
+}
